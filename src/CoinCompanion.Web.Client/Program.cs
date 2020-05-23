497a370fa164574ab3ae156615c72502766eb96e
Alexander Gebhard <39505376+a1g3@users.noreply.github.com>
2020-05-22 19:32:21 -0500

    Update NuGet packages.

--- src/CoinCompanion.Web.Client/Program.cs
+++ src/CoinCompanion.Web.Client/Program.cs
@@ -1,5 +1,7 @@
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Net.Http;
 using System.Threading.Tasks;
 
 namespace CoinCompanion.Web.Client
@@ -11,7 +13,11 @@ namespace CoinCompanion.Web.Client
             var builder = WebAssemblyHostBuilder.CreateDefault(args);
             builder.RootComponents.Add<App>("app");
 
-            builder.Services.AddBaseAddressHttpClient();
+            builder.Services.AddTransient(sp =>
+            new HttpClient
+            {
+                BaseAddress = new Uri(builder.HostEnvironment.BaseAddress)
+            });
 
             await builder.Build().RunAsync();
         }
